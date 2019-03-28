47690eea7b2d9edcf68adbe1e0e200403fa3be19
maxrchung <maxrchung@gmail.com>
2019-03-28 00:20:26 -0700

    Updated webpack to add hash to filename; separated CSS from JS file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,4 @@
+import "./main.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); 
+
 module.exports = {
   entry: "./src/main.jsx",
   module: {
@@ -7,13 +11,30 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader"
+        ]
       }
     ]
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "./build/"),
-    publicPath: "./build/",
-    filename: "bundle.js"
+    filename: "[name].[contenthash].js"
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.ejs"
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    })
+  ]
 };
