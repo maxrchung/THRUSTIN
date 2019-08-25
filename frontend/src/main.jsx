5a05c41ffc4309c736d11ecd42cc421a133d3ed6
maxrchung <maxrchung@gmail.com>
2019-08-25 15:14:46 -0700

    Woops, should probably make sure THRUSTY is a string

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -178,7 +178,7 @@ class Client extends React.Component {
 
     setMessage = message => {
         const data = JSON.stringify({
-            from: THRUSTY,
+            from: "THRUSTY",
             message: message
         });
        this.setJSON(data);
