ab335b44e4be171f972d9f22d8c3bb606e15f05b
maxrchung <maxrchung@gmail.com>
2022-01-08 03:16:34 -0800

    update to use server url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -62,7 +62,7 @@ class Client extends React.Component {
 
     componentDidMount() {
         if (process.env.NODE_ENV === "production") {
-            this.connection = new WebSocket("wss://thrustin.maxrchung.com:3012")
+            this.connection = new WebSocket("wss://server.thrustin.maxrchung.com")
         }
         else {
             this.connection = new WebSocket("ws://localhost:3012")
