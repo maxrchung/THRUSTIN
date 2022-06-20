27ad1089c361b657eb81dad716e661e82181d4d2
maxrchung <maxrchung@gmail.com>
2022-06-19 20:17:48 -0700

    set up debug logging

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,12 @@
+#[macro_use]
+extern crate log;
+
 extern crate dotenv;
 use dotenv::dotenv;
 use thrustin;
 
 fn main() {
     dotenv().ok();
+    env_logger::init();
     thrustin::run_ws_server();
 }
